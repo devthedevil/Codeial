2f23f2b196281c7458bae84c3ba4c22ee718e068
devthedevil <devpatna2016@gmail.com>
2024-01-03 16:01:22 -0600

    create view

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,11 @@
-module.exports.profile = function(res,res){
-    res.end('<h1>User Profile</h1>');
+// module.exports.profile = function(res,res){
+//     res.end('<h1>User Profile</h1>');
+// };
+module.exports.profile = function(req,res){
+    return res.render('users',{
+        title:"Profile of users"
+    });
 };
-
 // module.exports.users = function(res,res){
 //     res.end('<h1>Users</h1>');
 // };
