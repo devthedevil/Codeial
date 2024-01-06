c10008b6705c5f51dfe3deab737c20fcad9af60f
devthedevil <devpatna2016@gmail.com>
2024-01-05 21:53:03 -0600

    create layout

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User =require('../models/user');
 // module.exports.profile = function(res,res){
 //     res.end('<h1>User Profile</h1>');
 // };
@@ -28,7 +29,32 @@ module.exports.signIn = function(req,res){
 
 //get the sign up data
 module.exports.create = function(req,res){
-    //To do later
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email:req.body.email}).then((user)=>{
+        if(!user){
+            User.create(req.body).then(user =>{
+                return res.redirect('/users/sign-in');
+            }).catch((err)=>{
+            if(err){
+                console.log('Error in creating user while signing up');
+                return;
+            }
+        }); 
+        }else{
+            return res.redirect('back');
+        }
+    }).catch((err)=>{
+        if(err){
+            console.log('Error in finding user in signing up');
+            return;
+        }
+    });
+        
+        
+    
 }
 
 //get the sign in and create a session for the users
